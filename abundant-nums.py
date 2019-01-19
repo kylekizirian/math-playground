f7dcab81a94b227c2f3e4dc7580a89e17ab72f7e
kylekizirian <kyle.kizirian@gmail.com>
2019-01-19 16:46:44 -0500

    added a simple_get_divisors function that brute forces calculation of divisors

--- abundant-nums.py
+++ abundant-nums.py
@@ -2,7 +2,20 @@ import math
 import time
 
 
-def is_abundunt_num(num : int) -> bool:
+def simple_get_divisors(num : int) -> list:
+    '''Returns a list of divisors of num from 1 to n / 2
+    
+    Uses simplest method by checking all numbers from 1
+    to n / 2 to determine if they are a divisor of n
+    '''
+    all_divisors = []
+    for possible_divisor in range(1, math.floor(num / 2) + 1):
+        if num % possible_divisor == 0:
+            all_divisors.append(possible_divisor)
+    return all_divisors
+
+
+def simple_is_abundunt_num(num : int) -> bool:
     '''Takes in an integer and returns whether it is an abundant numuber
     
     Finds all divisors between 1 and n / 2 and sums them up, determining
@@ -11,18 +24,19 @@ def is_abundunt_num(num : int) -> bool:
     :param num: number to check if abundant
     :return: bool
     '''
-
-    assert isinstance(num, int) and num > 2, ('Input must be an int greater '
-                                              'than 2')
-
-    divisors = []
-    if num % 2 == 1:
-        for i in range(1, math.floor(num / 2) + 1, 2):
-            if num % i == 0:
-                divisors.append(i)
-    else:
-        for i in range(1, math.floor(num / 2) + 1):
-            if num % i == 0:
-                divisors.append(i)
-
+    divisors = simple_get_divisors(num)
     return sum(divisors) > num
+
+
+if __name__ == '__main__':
+    
+    range_to_check = 10**4
+
+    start_time = time.time()
+    for i in range(range_to_check):
+        simple_is_abundunt_num(i)
+    end_time = time.time()
+
+    print(f'Simple check for all abundant numbers from 0 to {range_to_check} '
+          f'took {end_time - start_time} seconds')
+
