7550956d806841169e583cf08b6adbf67b89025a
kylekizirian <kyle.kizirian@gmail.com>
2019-01-28 22:05:12 -0500

    removed unneeded functions

--- math_playground/primes.py
+++ math_playground/primes.py
@@ -1,44 +1,6 @@
 import math
 
 
-def get_list_of_primes_up_to(num: int) -> list:
-    """Returns list of primes up to num
-
-    >>> get_list_of_primes_up_to(20)
-    [2, 3, 5, 7, 11, 13, 17, 19]
-    """
-
-    all_primes = [2]
-    for i in range(3, num, 2):
-        upper_bound = math.sqrt(i)
-        for prime in all_primes:
-            if prime > upper_bound:
-                all_primes.append(i)
-                break
-            if i % prime == 0:
-                break
-    return all_primes
-
-
-def is_prime(num: int) -> bool:
-    """Returns whether or not input num is prime
-    
-    >>> is_prime(17)
-    True
-    >>> is_prime(21)
-    False
-    """
-
-    upper_bound = math.ceil(math.sqrt(num))
-    primes_up_to_upper_bound = get_list_of_primes_up_to(upper_bound)
-
-    for prime in primes_up_to_upper_bound:
-        if num % prime == 0:
-            return False
-    else:
-        return True
-
-
 class Primes:
 
     def __init__(self, up_to: int):
