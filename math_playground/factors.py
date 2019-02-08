5f00e7666d564c0a15d03f7cf8ef97c93998f067
kylekizirian <kyle.kizirian@gmail.com>
2019-02-07 19:23:28 -0500

    moved is_abundant_num method to factors module

--- math_playground/factors.py
+++ math_playground/factors.py
@@ -122,3 +122,23 @@ def get_sum_of_proper_divisors(num: int, prime_factors: list = None) -> int:
         sum_proper_divisors *= temp_sum
 
     return sum_proper_divisors - num
+
+def is_abundant_num(num: int, get_divisors_func=simple_get_divisors) -> bool:
+    """Takes in an integer and returns whether it is an abundant numuber
+    
+    Finds all divisors between 1 and n / 2 and sums them up, determining
+    if their sum is greater than n. If so, returns True, else False.
+
+    >>> is_abundant_num(24)
+    True
+    >>> is_abundant_num(37)
+    False
+    >>> is_abundant_num(24, get_divisors_with_parity_check)
+    True
+
+    :param num: number to check if abundant
+    :param get_divisors_func: function to use for getting num's divisors
+    :return: bool
+    """
+    divisors = get_divisors_func(num)
+    return sum(divisors) > num
