63f9bec445e436bc826115e43ec35e4e3c536fda
kylekizirian <kyle.kizirian@gmail.com>
2019-02-12 21:34:09 -0500

    added more unit tests for primes module

--- tests/test_primes.py
+++ tests/test_primes.py
@@ -1,3 +1,5 @@
+import timeit
+
 from math_playground import primes
 
 def test_len_primes():
@@ -8,3 +10,18 @@ def test_in_primes():
     primes_under_ten = primes.Primes(10)
     assert (3 in primes_under_ten)
     assert (4 not in primes_under_ten)
+
+def test_iter_primes():
+    primes_under_ten = primes.Primes(10)
+    primes_under_ten_list = [prime for prime in primes_under_ten]
+    assert (primes_under_ten_list == [2, 3, 5, 7])
+
+def test_reversed_primes():
+    primes_under_ten = primes.Primes(10)
+    primes_under_ten_reversed = [prime for prime in reversed(primes_under_ten)]
+    assert (primes_under_ten_reversed == [7, 5, 3, 2])
+
+def test_largest_prime():
+    primes_under_thousand = primes.Primes(10**3)
+    largest_prime_under_thousand = primes_under_thousand.largest_prime()
+    assert (largest_prime_under_thousand == 997)
