f464039bca75c7ceb779181db262e6f963ba3a84
kylekizirian <kyle.kizirian@gmail.com>
2019-06-24 19:11:44 -0400

    added hypothesis test for getting prime factors

---
+++ tests/test_factors.py
@@ -0,0 +1,13 @@
+from hypothesis import given, settings, strategies
+
+from pymathutils import factors
+
+
+@given(strategies.integers(min_value=1))
+@settings(deadline=None)
+def test_get_prime_factors(num):
+    prime_factors = factors.get_prime_factors(num)
+    result = 1
+    for prime_factor in prime_factors:
+        result *= prime_factor[0] ** prime_factor[1]
+    assert result == num
